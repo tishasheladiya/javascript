1156ec92932aade33f73985a7d8f570e7c3bf14b
tisha sheladiya <tishasheladiya@gmail.com>
2026-02-05 13:42:28 +0530

    array

---
+++ objects.js
@@ -0,0 +1,23 @@
+// Objects vs Array
+// Objects — when you want to show all data based on one entity(ek j entity mate all data jota hoy) (ex. one user data)
+// Array — when you want to show entity (ex. all users)
+
+// create
+// variable = {}
+// key : value --> key-value sturcture
+let user = {
+    name: "Test",
+    email: "test@gmail.com",
+    roll_no : 70
+}
+
+// access (dot vs square notation)
+// variable_name.key;
+user.name;
+user.email;
+// user.first-name;
+// variable_name['key'];
+// user['name']
+// user['first_name']
+// let demo = "name"
+// user[demo] --> convert into name (demo = 'name') --> output: abc
