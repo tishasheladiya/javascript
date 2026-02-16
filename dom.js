4f09570f35e13ee8e3efcbaeb5ffa7da80952c78
tisha sheladiya <tishasheladiya@gmail.com>
2026-02-16 12:18:47 +0530

    dom

--- dom.js
+++ dom.js
@@ -72,3 +72,64 @@ e.innerHTML = "<i>first h1 tag</i>"
 
 // innerHTML  -- show text between two tag -- not tag
 // outerHTML  -- show text with tag
+
+// -------------------------------------------------------------------------------------------------
+// Attribute manipulation
+// getAttribute , setAttribute, removeAttribute
+let link = document.querySelector("a");
+console.log(link);
+console.log(link.href);
+console.log("href value", link.getAttribute('href'))
+
+// setAttribute
+link.setAttribute("href", "https://www.google.com");
+console.log("href value", link.getAttribute('href'))
+
+// removeAttribute  
+link.removeAttribute("href");
+console.log("remove href value")
+
+//````````````````````````````````````````````````````````````
+// Dynamic DOM manipulation
+// createElement, appendChild, removeChild, prepend
+
+// createElement
+let text_h1 = document.createElement("h1");
+console.log(text_h1);
+text_h1.textContent = "create h1 with JS";
+
+// append / prepend
+// document.body.append(text_h1);
+document.body.prepend(text_h1);
+
+// appendChild
+let outer = document.createElement("div");
+outer.classList.add("outer");
+document.body.append(outer)
+
+let inner = document.createElement("div");
+inner.classList.add("inner");
+outer.appendChild(inner);
+// append vs appendChild
+// remove vs removeChild
+
+// --------------------------------------
+// Style Updates via .style and classList (add, remove, toggle)
+
+console.dir(text_h1);
+text_h1.style.backgroundColor = "black";
+text_h1.style.color = "white";
+text_h1.style.padding = "10px";
+
+
+// ClassList – add
+let card = document.querySelector(".card");
+console.dir(card);
+card.classList.add("card-style");
+
+// remove
+card.classList.remove("card");
+
+// toggle — class --> available --> remove it
+//          class --> not available --> add it
+card.classList.toggle("card");
